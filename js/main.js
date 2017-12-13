7ae1338e762688248837416c5fed46115fc51bf0
Ncmocha <nickichenbattle@gmail.com>
2017-12-13 12:28:57 -0600

    adding main.js to the project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+  //hover event handler on mouseover and mouseleave
+  $(".heading").hover(function(){
+    $(this).css('color','#ebc000');
+
+  },function(){
+    $(this).css('color','#fff');
+  });
+});
